ab1caee3c0099cbf5c299bdba252d242b0fb9ad4
Eduardo Morais <emorais1375@gmail.com>
2018-08-07 18:14:28 -0400

    Renomeia template

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -37,6 +37,6 @@ def create_app(test_config=None):
 
     from . import blog
     app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
+    app.add_url_rule('/', endpoint='index1')
 
     return app
