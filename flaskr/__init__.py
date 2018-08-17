9b15d2cf7dea23e9366542bf9bd5b15a24f2d6ea
Diogo Duarte <duartediogo98@gmail.com>
2018-08-16 21:41:00 -0400

    pagina base, pagina lista de labs e detalhe de labs

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-
+from flask import render_template, url_for
 
 def create_app(test_config=None):
     # create and configure the app
@@ -24,10 +24,33 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    def dated_url_for(endpoint, **values):
+        """dated_url_for."""
+        if endpoint == 'static':
+            filename = values.get('filename', None)
+            if filename:
+                file_path = os.path.join(app.root_path, endpoint, filename)
+                values['_'] = int(os.stat(file_path).st_mtime)
+        return url_for(endpoint, **values)
+
+    @app.context_processor
+    def override_url_for():
+        """override_url_for."""
+        return dict(url_for=dated_url_for)
+
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/base')
+    def base():
+        return render_template('base.html')
+
+    @app.route('/laboratorios')
+    def laboratorios():
+        return render_template('laboratorios.html')
+
+    @app.route('/detalhes')
+    def detalhes():
+        return render_template('lab_detalhes.html')
+
 
     from . import db
     db.init_app(app)
@@ -38,5 +61,4 @@ def create_app(test_config=None):
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index1')
-
     return app
